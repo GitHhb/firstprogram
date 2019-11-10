3ef73088632a8395b6af19f1362d0f7515cf3e6a
leandro <leandro@dagostino.nl>
2019-11-10 06:49:04 +0100

    ASML challenge works OO version

--- src/main/java/asml/codility/Solution.java
+++ src/main/java/asml/codility/Solution.java
@@ -6,38 +6,80 @@ package asml.codility;
 // you can write to stdout for debugging purposes, e.g.
 // System.out.println("this is a debug message");
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 class Solution {
     public int solution(int[] T, boolean[] B) {
         // write your code in Java SE 8
-
         // amount of plutonium the ship can hold at maximum
-        final int MAX_PLUTONIUM = 3;
-
         // Init
-        int sp;
-        int count = 0;
+        final int MAX_PLUTONIUM = 3;
+        final int INITIAL_PLUTONIUM = MAX_PLUTONIUM;
+        int startpoint;
+        int nrReachablePlanets = 0;
 
-        // get starting point
+        class Planet {
+            List<Integer> nextPlanets = new ArrayList<>();
+        }
+        // planets[i] contains next planets
+        Planet[] planets = new Planet[T.length];
+        // hasFuel[i] indicates fuel availability
+        boolean[] hasFuel = B;
+
+        class SpaceCraftStop {
+            int planet; // current position
+            int fuel;
+
+            SpaceCraftStop(int planet, int fuel) {
+                System.out.printf("next %2d = fuel %2d\n", planet, fuel);
+
+                this.planet = planet;
+                // refuel if this planet has fuel
+                this.fuel = Integer.max(fuel, hasFuel[planet] ? MAX_PLUTONIUM : 0);
+            }
+        }
+        // Track position and fuel amount
+        List<SpaceCraftStop> spaceCraftStops = new ArrayList<>();
+
+        // create graph of planets and get starting point
         for (int i = 0; i < T.length; i++) {
+            if (planets[T[i]] == null) {
+                planets[T[i]] = new Planet();
+            }
+            planets[T[i]].nextPlanets.add(i);
+
             if (i == T[i]) {
-                sp = i;
-                count = 1;
-                break;
+                nrReachablePlanets = 1;
+                spaceCraftStops.add(new SpaceCraftStop(i, INITIAL_PLUTONIUM));
             }
         }
 
+        int currentPlanet, currentFuel;
+        SpaceCraftStop newStop;
         // if there is no starting point, STOP
-        if (count == 0) {
-            return count;
+        while (spaceCraftStops.size() > 0) {
+            currentPlanet = spaceCraftStops.get(0).planet;
+            currentFuel = spaceCraftStops.get(0).fuel;
+            System.out.printf("curPlanet %2d = fuel %2d\n", currentPlanet, currentFuel);
+            spaceCraftStops.remove(0);
+            // are there any successors for this planet?
+            if (Objects.nonNull(planets[currentPlanet])) {
+                // create stop for every planet that is reachable from the current planet
+                for (int nextPlanet : planets[currentPlanet].nextPlanets) {
+                    // don't go back to the planet we came from
+                    if (nextPlanet == currentPlanet || currentFuel == 0) {
+                        continue;
+                    }
+                    newStop = new SpaceCraftStop(nextPlanet, currentFuel - 1);
+                    nrReachablePlanets++;
+                    spaceCraftStops.add(newStop);
+                }
+            }
         }
 
-        // search reachable paths
-
-
-
-        return 100;
-
-
+        return nrReachablePlanets;
     }
 }
 
