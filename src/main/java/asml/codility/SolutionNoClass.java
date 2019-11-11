17f016e75a997d5fed1c4aee69b566d801f0c14b
leandro <leandro@dagostino.nl>
2019-11-11 02:10:22 +0100

    ASML challenge works OO version

--- src/main/java/asml/codility/SolutionNoClass.java
+++ src/main/java/asml/codility/SolutionNoClass.java
@@ -4,20 +4,36 @@ public class SolutionNoClass {
     public int solution(int[] T, boolean[] B) {
         int[] countOccur = new int[T.length];
         for (int i = 0; i < T.length; i++) {
-            countOccur[i]++;
+            countOccur[T[i]]++;
         }
+        printit(T);
+        printit(countOccur);
 
         int[] index = new int[T.length];
         for (int i = 1; i < T.length; i++) {
             index[i] = index[i - 1] + countOccur[i - 1];
+            countOccur[i - 1] = index[i - 1];
+        }
+        if (T.length > 0) {
+            countOccur[T.length - 1] = index[T.length - 1];
         }
 
         int[] nodes = new int[T.length];
-        for (int i = 1; i < T.length; i++) {
-//            nodes[index[T[i]]]
+        for (int i = 0; i < T.length; i++) {
+            nodes[countOccur[T[i]]++] = i;
         }
 
+        printit(countOccur);
+        printit(index);
+        printit(nodes);
         return 0;
 
+    }
+
+    void printit(int[] x) {
+        for (int i = 0; i < x.length; i++) {
+            System.out.printf("%2d ", x[i]);
+        }
+        System.out.println();
     }
 }
--- src/main/java/asml/codility/TestSolution.java
+++ src/main/java/asml/codility/TestSolution.java
@@ -2,21 +2,23 @@ package asml.codility;
 
 public class TestSolution {
     public static void main(String[] args) {
+        int[] planets2 = new int[]{0, 0, 1, 2, 3};
+        boolean[] plutonium2 = {false, false, false, false, false};
+        System.out.println(new Solution().solution(planets2, plutonium2));
+        System.out.println(new SolutionNoClass().solution(planets2, plutonium2));
+
         int[] planets = {9, 1, 4, 9, 0, 4, 8, 1, 0, 7};
         boolean[] plutonium = {false, false, false, false, false, false, false, true, false, false};
         System.out.println(new Solution().solution(planets, plutonium));
-
-        int[] planets2 = new int[]{0, 0, 1, 2, 3};
-        boolean[] plutonium2 = { false, false, false, false, false};
-        System.out.println(new Solution().solution(planets2, plutonium2));
-
-        int[] planets1 = new int[]{7, 2, 2, 7, 0, 1, 5, 2};
-        boolean[] plutonium1 = { false, false, false, true, false, false, true, true };
-        System.out.println(new Solution().solution(planets1, plutonium1));
-
-        int[] planets3 = new int[]{};
-        boolean[] plutonium3 = { };
-        System.out.println(new Solution().solution(planets3, plutonium3));
+        System.out.println(new SolutionNoClass().solution(planets, plutonium));
+//
+//        int[] planets1 = new int[]{7, 2, 2, 7, 0, 1, 5, 2};
+//        boolean[] plutonium1 = {false, false, false, true, false, false, true, true};
+//        System.out.println(new Solution().solution(planets1, plutonium1));
+//
+//        int[] planets3 = new int[]{};
+//        boolean[] plutonium3 = {};
+//        System.out.println(new Solution().solution(planets3, plutonium3));
 
     }
 }
